65bf0c83afa59b8e3a706cf0c28d92157f0fb161
Maxwell Croy <max.croy1@gmail.com>
2020-05-22 14:18:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  []
+  newArray = []
 end
 
 def create_an_array
-  
+  family = ["Max","Samantha","Jeff","Jennifer"]
 end
 
 def add_element_to_end_of_array(array, element)
